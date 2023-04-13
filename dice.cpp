c731e1c52c06ece93e50449cfda0069faadbd651
Dylan Sizemore <riverrock94@hotmail.com>
2023-04-12 20:21:17 -0400

    updated line21

--- dice.cpp
+++ dice.cpp
@@ -6,20 +6,22 @@ using namespace std;
 int CheckPrediction(){
     int choice=0;
     cin >> choice;
-    if (choice>6){
-        cout << "Choose Number 1-6" << endl;
+   if (choice>6)
+   {
+        cout << "Choose a Number 1-6" << endl;
         CheckPrediction();
     }
-    return choice;
+    return choice; 
     
 }
 int main ()
 {
     cout << "Enter Predictions!" << endl; 
     int choice;
+    cin >> choice;
     CheckPrediction();
     srand(time(NULL));
-    cout <<"Dice Roll" << endl;
+    cout <<"Dice Rolled!!" << endl;
     int roll = 0;
     roll= rand() % 6 + 1;
     cout <<"Your Prediction Was " << choice << endl;
