b69c792a57a956eafabae41dc62ef8a48d06415c
Dylan Sizemore <riverrock94@hotmail.com>
2023-04-13 14:43:56 -0400

    for loop lne 45

--- dice.cpp
+++ dice.cpp
@@ -7,6 +7,7 @@ int CheckPrediction(int choice){
    if (choice>6)
    {
         cout << "Choose a Number 1-6" << endl;
+        for (int i=0; i<3; i++)
         CheckPrediction(choice);
     }
     return choice; 
@@ -15,6 +16,7 @@ int CheckPrediction(int choice){
 int DiceRoll(){
     int roll = rand() % 6 + 1;
     return roll;
+    
 }
 
 int main ()
@@ -31,19 +33,19 @@ int main ()
     // i<3;       "i" will always remain less than 3
     // i++        Every time it loops "i" will increase by an increment of 1
     // Note: When coding, numbers will start with 0 NOT 1
-    for (int i=0; i<3; i++){
-        cout << i << endl;
-    }
+
+    
     // If you build and run this, you will see "i" being printed out as 0, 1, 2
     // We want to roll the dice 3 times and whatever you put into this loop will run 3 times
     // Happy hunting! 
 
     cout << "Enter prediction of dice roll" << endl; 
     cin >> choice;
-    CheckPrediction(choice);
-
     cout << "Dice Rolled!!" << endl;
     int roll = DiceRoll();
+    for (int i=0; i<3; i++){
+        cout << rand() % 6 + 1 << endl;
+    }
     
     cout << "Your Prediction Was " << choice << endl;
     cout << "Your Roll Was " << roll << endl;
